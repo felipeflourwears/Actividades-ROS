f8c713c1da79c3354e1174f928c5163e6651b3a5
felipeflourwears <A01274880@tec.mx>
2023-03-29 16:51:11 -0700

    Turtlebots and Suscriber Odometry

---
+++ src/mypkg/src/mynode.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+import rospy
+from std_msgs.msg import Float64
+from nav_msgs.msg import Odometry
+from tf.transformations import euler_from_quaternion, quaternion_from_euler
+
+def radians_to_degrees(radians):
+    return radians * 180 / 3.141592
+
+def callback(msg, publisher):
+    x = msg.pose.pose.orientation.x
+    y = msg.pose.pose.orientation.y
+    z = msg.pose.pose.orientation.z
+    w = msg.pose.pose.orientation.w
+    
+    roll, pitch, yaw = euler_from_quaternion([x, y, z, w])
+    grados = radians_to_degrees(yaw)
+    
+    publisher.publish(grados)  # Publicar la variable "grados" en el nuevo nodo
+    
+    rospy.loginfo("GRADOS: %f, RADIANES: %f", grados, yaw)
+
+def listener():
+    rospy.init_node('subscritor', anonymous=True)
+
+    pub = rospy.Publisher('mynodedegrees', Float64, queue_size=10)  # Crear publicador en el nuevo nodo
+    
+    rospy.Subscriber("odom", Odometry, callback, pub)
+
+    rospy.spin()
+
+if __name__ == '__main__':
+    try:
+        listener()
+    except rospy.ROSInterruptException:
+        pass
